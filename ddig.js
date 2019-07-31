7ae4289cb8f1bd9d430da02e0e2786f98376dba8
Mark Murphy <mark.murphy@asos.com>
2019-07-31 15:55:09 +0100

    Further refactoring of recursive lookup functions

--- ddig.js
+++ ddig.js
@@ -25,6 +25,64 @@ var options = {
 };
 
 module.exports = {
+    // resolve() issues a single DNS lookup request
+    resolve(domain, resolver) {
+        // Create DNS Question
+        var question = dns.Question({
+            name: domain,
+            type: options.question.type,
+        });
+
+        // Create DNS Request to ask the Question
+        var req = dns.Request({
+            question: question,
+            server: {
+                address: resolver.nameServer,
+                port: options.request.port,
+                type: options.request.type
+            },
+            timeout: options.request.timeout,
+            cache: options.request.cache,
+            try_edns: options.request.try_edns
+        });
+
+        // Issue DNS request
+        req.send();
+        // **** DNS resolution event hook functions ****
+        // Handle a DNS timeout event
+        req.on('timeout', function () {
+            console.log('The %sms timeout elapsed before %s [%s] responded', options.request.timeout, resolver.nameServer, resolver.provider);
+        });
+
+        // Handle DNS message event; i.e process the `answer` response
+        req.on('message', function (err, answer) {
+            if (err) {
+                debug('Error received: %O', err);
+            } else{
+                debug('The resolver [%s] provided the answer: %O', resolver.nameServer, answer.answer);
+                answer.answer.forEach(function (a) {
+                    debug(a.data);
+                });
+
+                // Create lookup result object
+                var lookupResult = {
+                    'domain' : domain,
+                    'ipAddress' : [JSON.stringify(answer.answer)],
+                    'resolver' : resolver.nameServer,
+                    'provider' : resolver.provider
+                };
+
+                // Return the result
+                return(lookupResult);
+            }
+        });
+
+        // The `end` event fires at the end of each DNS request regardless of its success
+        req.on('end', function () {
+            debug('Finished processing DNS request');
+        });
+    },
+
     // resolveDomain() iterates through the resolvers performing a lookup of a single domain
     // Returns json object
     resolveDomain(domain, resolvers) {
@@ -38,60 +96,9 @@ module.exports = {
             if (isIp(resolvers[index].nameServer) === false) {
                 debug('Skipping the resolver [%s] because it is not a valid IP address', resolvers[index].nameServer);
             } else {
-                // Create DNS Question
-                var question = dns.Question({
-                    name: domain,
-                    type: options.question.type,
-                });
-
-                // Create DNS Request to ask the Question
-                var req = dns.Request({
-                    question: question,
-                    server: {
-                        address: resolvers[index].nameServer,
-                        port: options.request.port,
-                        type: options.request.type
-                    },
-                    timeout: options.request.timeout,
-                    cache: options.request.cache,
-                    try_edns: options.request.try_edns
-                });
-
-                // Hook the timeout event
-                req.on('timeout', function () {
-                    console.log('The %sms timeout elapsed before %s [%s] responded', options.request.timeout, resolvers[index].nameServer, resolvers[index].provider);
-                });
-
-
-                req.on('message', function (err, answer) {
-                    if (err) {
-                        debug('Error received: %O', err);
-                    } else{
-                        debug('The resolver [%s] provided the answer: %O', resolvers[index].nameServer, answer.answer);
-                        answer.answer.forEach(function (a) {
-                            debug(a.data);
-                        });
-
-                        // Create lookup result object
-                        var lookupResult = {
-                            'domain' : domain,
-                            'ipAddress' : [JSON.stringify(answer.answer)],
-                            'resolver' : resolvers[index].nameServer,
-                            'provider' : resolvers[index].provider
-                        };
-
-                        // Return the result
-                        return(lookupResult);
-                    }
-                });
-
-                req.on('end', function () {
-                    debug('Finished processing DNS request');
-                });
-
-                req.send();
+                var lookup = module.exports.resolve(domain, resolvers[index]);
+                return(lookup);
             }
-
         }
     },
 
@@ -108,7 +115,7 @@ module.exports = {
         };
 
         for (var iDomain = 0; iDomain < domains.length; iDomain++) {
-            var result = exports.resolveDomain(domains[iDomain], resolvers);
+            var result = module.exports.resolveDomain(domains[iDomain], resolvers);
             if (result) {
                 response.lookups.push(result);
             }
